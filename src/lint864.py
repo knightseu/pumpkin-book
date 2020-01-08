d7388d42abd3b26f22c7cdd75243404b9ff94b0e
seanxl <sean.xuanlin@gmail.com>
2020-01-08 05:24:28 +0000

    add 864 partition equal tree

---
+++ src/lint864.py
@@ -0,0 +1,15 @@
+from common.tree import TreeNode
+
+mp = {}
+def partitionEqualTree(root):
+  sum = transversal(root)
+  return sum%2 == 0 and sum/2 in mp
+
+def transversal(root):
+  if root == None:
+    return 0
+  sum = root.data + transversal(root.left) + transversal(root.right) 
+  if not sum in mp:
+    mp[sum] = 1
+  return sum
+  

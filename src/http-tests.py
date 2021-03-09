17561011f33178ea92ed42dfeea09118aa192b89
Sean Lin <sean.lin@jpmchase.com>
2021-03-09 10:57:25 -0800

    http-test

--- src/http-tests.py
+++ src/http-tests.py
@@ -1,11 +1,16 @@
 # http tests
 import requests
+import json
+
 from pprint import pprint
 dest = "https://api.github.com/users/knightseu"
 r =requests.get(dest)
 
 pprint(r.headers)
-
+# parsed = json.dumps(r.headers, indent = 4)
+# pprint(parsed)
+for a, b in r.headers.items():
+    print("{}: {}".format(a, b))
 print("------\n\n\n")
 
 pprint(r.text)
